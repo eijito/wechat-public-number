4522ee35772afe802973161669a1e6577d06b61f
xpg19 <eijito@foxmail.com>
2018-05-31 17:37:23 +0800

    1

---
+++ src/main/java/com/czsnts/wechatpublicnumber/WechatPublicNumberApplication.java
@@ -0,0 +1,12 @@
+package com.czsnts.wechatpublicnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatPublicNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatPublicNumberApplication.class, args);
+	}
+}
---
+++ src/main/java/org/weixin4j/component/MaterialComponent.java
@@ -0,0 +1,15 @@
+package org.weixin4j.component;
+
+import org.weixin4j.Weixin;
+
+/**
+ * 素材组件
+ */
+public class MaterialComponent extends AbstractComponent {
+
+    public MaterialComponent(Weixin weixin) {
+        super(weixin);
+    }
+
+    
+}
